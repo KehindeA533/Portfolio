ec2567e140df367298d5fafd2ed0a2e1e22bab47
Kehinde. A <whenkehindewalks@gmail.com>
2022-12-02 01:58:41 -0500

    update resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ export default function CTA() {
 
     return (
         <div className="cta">
-            <a href={CV} className="btn">Download CV</a>
+            <a href="https://resume.io/r/eP3KOdJC4" className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Lets's Talk</a>
         </div>
     )
